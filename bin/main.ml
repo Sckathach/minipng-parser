d27c299cacff9a4afdefa2976d04bba0e00b79c0
Sckathach <sckathach@hackademint.org>
2023-12-15 20:03:51 +0100

    display functions now output a string

--- bin/main.ml
+++ bin/main.ml
@@ -8,25 +8,35 @@ open Minipng
 *)
 
 let display_header header =
-    Printf.printf "Width: %d\n" header.width;
-    Printf.printf "Height: %d\n" header.height;
-    Printf.printf "Pixel Type: %d " header.pixel_type;
-    match header.pixel_type with
-        0 -> print_endline "(black-and-white)"
-        | _ -> print_endline "(other types not implemented)"
+    let result = ref "" in
+    result := !result ^ Printf.sprintf "Width: %d\n" header.width;
+    result := !result ^ Printf.sprintf "Height: %d\n" header.height;
+    result := !result ^ Printf.sprintf "Pixel Type: %d " header.pixel_type;
+    begin
+        match header.pixel_type with
+            0 -> result := !result ^ "(black-and-white)\n"
+            | _ -> result := !result ^ "(other types not implemented)\n"
+    end;
+    !result
 
 let display_data width data =
+    let result = ref "" in
     for i = 0 to String.length data - 1 do
         if i <> 0 && i mod width = 0 then
-            print_newline ();
-        Printf.printf "%c" (String.get data i)
-    done
+            result := !result ^ "\n" ;
+        result := !result ^ Printf.sprintf "%c" (String.get data i)
+    done;
+    !result
+
+let display_comment comment =
+    Printf.sprintf "Comments:\n\"%s\"\n" comment
 
 
 let () =
-    let filename = Sys.argv.(1) in
+(*    let filename = Sys.argv.(1) in *)
+    let filename = "minipng-samples/bw/ok/A.mp" in
     let blocks = parse_file filename in
     let minipng = blocks_to_minipng blocks in
-    display_header minipng.header;
-    Printf.printf "Comments:\n\"%s\"\n" minipng.comment;
-    display_data minipng.header.width (transform_string minipng.data);
+    Printf.printf "%s" (display_header minipng.header);
+    Printf.printf "%s" (display_comment minipng.comment);
+    Printf.printf "%s" (display_data minipng.header.width (transform_string minipng.data));
--- test/test_parser.ml
+++ test/test_parser.ml
@@ -1,2 +1,16 @@
+open Minipng
+open Filename
+
+(*Idk why it doesn't work with relative PATH...*)
+let path = "/mnt/Documents/INT/TSP/Cours/2A/csc4203/parser/test/minipng-samples/"
+
+let%test "wrong magic" =
+    let filename = path ^ "bw/ok/A.mp" in
+    try
+        let blocks = parse_file filename in
+        true
+    with
+        Failure x ->
+            x = "Invalid magic number"
+
 let%test _ = true
-let%test _ = false
